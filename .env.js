96b83d001f76f67df4d5f661327ea260ec577bb3
jronak813 <51308882+jronak813@users.noreply.github.com>
2023-02-07 14:37:35 +0530

    Update .env.js

--- .env.js
+++ .env.js
@@ -8,7 +8,7 @@ const development = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     PORT: process.env.PORT || '9000',
     Meta_WA_accessToken:
-        'EAAKBQIWASUwBACKlcvB3i40SYwYleyyZCs3F4JfVBLNUuljMpOoFKXUUK0uxEcbYTgcrdXLSZCoQLrBxV2ej0LSpHha3myMiFeGpc6TYZBGEdQkl6H4xxX5mc1t8bv4yT0rHMPEK91cZButQ3ZAhm9kNZAdtW4KQGWALDpuDAAUaUS6eNIFq5RDisKEX4nLCJWeIhpfrcQROHZAClZBe1zBW',
+        'EAAKBQIWASUwBAGRu5MgbsRMac98KlClphfd1cXcy46G2h7RVHtVssufMJVszX8elw6CN9Yzc5hKvdaF3zip2L6nf6dDxKQa7ZAxZA2SyjXKyFZCTGxZBrDZC8ZAZA35VSVDZBUPP4TEOkPslWxK6xSzZB5bkmuoA2wqtBgM2At2ZBi7bcTeenMO5tEQextIpcfOEVWmhfrrg1VqUf4NTVndlaa',
     Meta_WA_SenderPhoneNumberId: '101580936177175',
     Meta_WA_wabaId: '104193235911486',
     Meta_WA_VerifyToken: '12345',
